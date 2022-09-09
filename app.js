066f510d8e62a4ba8426910f755984d00ec684f9
belgonzalez21 <nsbelg@gmail.com>
2022-09-09 08:25:18 -0300

    register y login finalizado

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(express.static('public'));
 app.get('/', (req,res) => {
     return res.sendFile(path.resolve(__dirname,'views','index.html'))
 });
+app.get('/register', (req,res) => {
+    return res.sendFile(path.resolve(__dirname,'views','register.html'))
+});
+app.get('/login', (req,res) => {
+    return res.sendFile(path.resolve(__dirname,'views','login.html'))
+});
 
 app.get('*', (req,res) => {
     return res.sendFile(path.resolve(__dirname, 'views', 'notFound.html'))
